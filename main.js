616541d7dc51c0c59baba74bc3f2d9def227cec6
Pau Egea <papau84@gmail.com>
2022-04-30 19:47:13 +0200

    edit js paginate

--- main.js
+++ main.js
@@ -1,28 +1,17 @@
-const url = "https://api.thecatapi.com/v1/images/search";
-// const button = document.querySelector("button");
-const img = document.querySelector("img"); // getting the image element
-
-// const createImg = () => {
-//   fetch(url) // fetching the data from the API
-//     .then((response) => response.json()) // converting the data to JSON
-//     .then((data) => {
-//       const cat = data[0].url; // getting the first cat from the array
-//       img.setAttribute("src", cat); // setting the image source
-//     });
-// };
-// createImg();
-
-// const change = () => {
-//   img.removeAttribute("src"); // removing the image source
-//   createImg();
-// };
-
-// button.addEventListener("click", change);
+const url = "https://api.thecatapi.com/v1/images/search?limit=3";
 
 async function reload() {
-    const response = await fetch(url);
-    const data = await response.json();
-    img.src = data[0].url;
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+  const img1 = document.getElementById("img1");
+  const img2 = document.getElementById("img2");
+  const img3 = document.getElementById("img3");
+  img1.src = data[0].url;
+  img2.src = data[1].url;
+  img3.src = data[2].url;
+
+  //   img.src = data[0].url;
 }
 
 reload();
