b0d4b220e4c7190cfde2352d98611e1dca0df6e7
Pau Egea <papau84@gmail.com>
2022-05-01 09:25:45 +0200

    edit html js and favourites

--- main.js
+++ main.js
@@ -1,22 +1,75 @@
-import Apikey from "./env.js";
-const url = "https://api.thecatapi.com/v1/images/search?limit=3&api_key="+Apikey;
-const button = document.querySelector("button");
-console.log(button)
+const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=37319f20-acfe-4034-8677-4996b1321da4';
+const API_URL_FAVOTITES = 'https://api.thecatapi.com/v1/favourites?api_key=37319f20-acfe-4034-8677-4996b1321da4';
 
+const spanError = document.getElementById('error')
 
-async function reload() {
-  const response = await fetch(url);
-  const data = await response.json();
-  console.log(data);
-  const img1 = document.getElementById("img1");
-  const img2 = document.getElementById("img2");
-  const img3 = document.getElementById("img3");
-  img1.src = data[0].url;
-  img2.src = data[1].url;
-  img3.src = data[2].url;
+async function loadRandomMichis() {
+  const res = await fetch(API_URL_RANDOM);
+  const data = await res.json();
+  console.log('Random')
+  console.log(data)
 
-  //   img.src = data[0].url;
+  if (res.status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + res.status;
+  } else {
+    const img1 = document.getElementById('img1');
+    const img2 = document.getElementById('img2');
+    const btn1 = document.getElementById('btn1');
+    const btn2 = document.getElementById('btn2');
+    
+    img1.src = data[0].url;
+    img2.src = data[1].url;
+
+    btn1.onclick = () => saveFavouriteMichi(data[0].id);
+    btn2.onclick = () => saveFavouriteMichi(data[1].id);
+  }
 }
 
-reload();
-button.addEventListener("click", reload());
+async function loadFavouriteMichis() {
+  const res = await fetch(API_URL_FAVOTITES);
+  const data = await res.json();
+  console.log('Favoritos')
+  console.log(data)
+
+  if (res.status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + res.status + data.message;
+  } else {
+    data.forEach(michi => {
+      const section = document.getElementById('favoriteMichis')
+      const article = document.createElement('article');
+      const img = document.createElement('img');
+      const btn = document.createElement('button');
+      const btnText = document.createTextNode('Sacar al michi de favoritos');
+
+      img.src = michi.image.url;
+      img.width = 150;
+      btn.appendChild(btnText);
+      article.appendChild(img);
+      article.appendChild(btn);
+      section.appendChild(article);
+    });
+  }
+}
+
+async function saveFavouriteMichi(id) {
+  const res = await fetch(API_URL_FAVOTITES, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      image_id: id
+    }),
+  });
+  const data = await res.json();
+
+  console.log('Save')
+  console.log(res)
+
+  if (res.status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + res.status + data.message;
+  }
+}
+
+loadRandomMichis();
+loadFavouriteMichis();
